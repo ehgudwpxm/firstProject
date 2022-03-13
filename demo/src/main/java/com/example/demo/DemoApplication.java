2e1e9e188fc2ef153dbb90de271e8dd197be425f
Lee dohyeong <alsgudwpxm31@gmail.com>
2022-03-13 19:06:08 +0900

    [TEST] Commit!!!

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("dddd");
+		//됬나
+		// 되어마어람;ㅇㄴ라ㅓㄴㅁ
+		// 커밋이되었나...
+	}
+
+}
